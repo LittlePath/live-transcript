8862897cbfde51d33ad97cb0b8acf930bfad33f8
Scott Davis <scott@thirstyhead.com>
2020-09-09 17:35:40 -0600

    working example; needs polish

--- components/live-transcript/index.js
+++ components/live-transcript/index.js
@@ -12,6 +12,7 @@ window.customElements.define('live-transcript',
       let startStopButton = this.shadowRoot.querySelector('#start-stop');
       startStopButton.addEventListener('click', (event) => this.startStopClickHandler(event));
 
+      this.recognition = undefined;
       this.transcribing = false;
     }
 
@@ -26,6 +27,11 @@ window.customElements.define('live-transcript',
           width: 50%;
           height: 50%;
           overflow-y: scroll;
+          font-style: italic;
+        }
+
+        .final{
+          font-style: normal;
         }
       `;
       return style;
@@ -42,26 +48,55 @@ window.customElements.define('live-transcript',
     }
 
     startStopClickHandler(event){
-      let startStopButton = this.shadowRoot.querySelector('#start-stop');
-      if(startStopButton.innerHTML.startsWith('Start')){
-        startStopButton.innerHTML = 'Stop Transcript';
-        startStopButton.classList.add('stop');
-        this.transcribe();
+      if(this.transcribing){
+        this.stop();
       }else{
-        startStopButton.innerHTML = 'Start Transcript';
-        startStopButton.classList.remove('stop');
+        this.start();
       }
     }
 
-    transcribe(){
+    start(){
       const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-      if (typeof SpeechRecognition === "undefined") {
+      if(typeof SpeechRecognition === "undefined"){
         this.write(`This browser doesn't support the <a href="https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition">SpeechRecognition</a> API. Try Google Chrome, Microsoft Edge, or one of the browsers listed at <a href="https://caniuse.com/mdn-api_speechrecognition">caniuse.com</a>.`);  
+        let startStopButton = this.shadowRoot.querySelector('#start-stop');
+        startStopButton.disabled = true;
+      }else{
+        this.recognition = new SpeechRecognition();
+        this.recognition.continuous = true;
+        this.recognition.interimResults = true;
+        this.recognition.addEventListener('result', (event) => this.resultHandler(event));
+        this.recognition.start();
+        this.transcribing = true;
       }
     }
 
-    write(message){
+    stop(){
+      let startStopButton = this.shadowRoot.querySelector('#start-stop');
+      startStopButton.innerHTML = 'Start Transcript';
+      startStopButton.classList.remove('stop');
+      this.recognition.stop();
+      this.transcribing = false;
+    }
+
+    resultHandler(event){
       let transcriptWindow = this.shadowRoot.querySelector('#transcript-window');
+      transcriptWindow.innerHTML = '';
+      for(const result of event.results){
+        if(result.isFinal){
+          this.write(result[0].transcript, 'final');
+        }else{
+          this.write(result[0].transcript);
+        }
+      }
+    }
+
+    write(message, className){
+      let transcriptWindow = this.shadowRoot.querySelector('#transcript-window');
+      let p = document.createElement('p');
+      if(className){
+        p.classList.add(className);
+      }
       transcriptWindow.insertAdjacentHTML('beforeend', `<p>${message}</p>`);
     }
 
